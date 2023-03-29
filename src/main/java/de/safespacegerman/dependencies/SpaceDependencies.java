7f219fad38de224f785d4dc3c89d049ea6f8fc46
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2023-03-29 23:56:25 +0200

    Dependency Init

---
+++ src/main/java/de/safespacegerman/dependencies/SpaceDependencies.java
@@ -0,0 +1,28 @@
+package de.safespacegerman.dependencies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * SpaceDependencies; de.safespacegerman.dependencies:SpaceDependencies
+ *
+ * @author <a href="https://github.com/LuciferMorningstarDev">LuciferMorningstarDev</a>
+ * @since 29.03.2023
+ */
+public class SpaceDependencies extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
